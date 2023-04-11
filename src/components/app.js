aab9088c36803d611d2c3f36dda1fb471c5ef90a
Greg Cain <codegregcode@gmail.com>
2023-04-11 13:28:38 +0100

    creates SearchResults component and tests and styles, imports into app, updates snapshot

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Search from "./Search";
+import SearchResults from "./SearchResults";
 import "../styles/app.css";
 import logo from "../assets/200424060716-nasa-worm-logo-removebg-preview.png";
 
@@ -11,6 +12,7 @@ const App = () => {
       <div className="app">
         <img className="nasa-logo" src={logo} alt="nasaLogo" />
         <Search setSearchResults={setSearchResults} />
+        <SearchResults results={searchResults} />
       </div>
     </div>
   );
