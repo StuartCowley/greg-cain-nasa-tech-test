a307c85a046153c09e3327effc9976c6ac05e89e
Greg Cain <codegregcode@gmail.com>
2023-04-11 13:22:35 +0100

    adds Search comoponent and tests, and styling,creates getImages, updates packagejson for axios import

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import Search from "./Search";
 import "../styles/app.css";
 import logo from "../assets/200424060716-nasa-worm-logo-removebg-preview.png";
 
 const App = () => {
+  const [searchResults, setSearchResults] = useState([]);
+
   return (
     <div className="bg">
       <div className="app">
         <img className="nasa-logo" src={logo} alt="nasaLogo" />
+        <Search setSearchResults={setSearchResults} />
       </div>
     </div>
   );
