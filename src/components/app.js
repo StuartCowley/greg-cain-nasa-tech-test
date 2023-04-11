af826101d81c2dc759d6621fca7724830dcda382
Greg Cain <codegregcode@gmail.com>
2023-04-11 13:05:46 +0100

    creates App component and tests, adds logo asset, and adds styles

--- src/components/app.js
+++ src/components/app.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "../styles/app.css";
+import logo from "../assets/200424060716-nasa-worm-logo-removebg-preview.png";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg">
+      <div className="app">
+        <img className="nasa-logo" src={logo} alt="nasaLogo" />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
